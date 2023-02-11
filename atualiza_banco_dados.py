9537e7d00cb711817a5e798047fa2d5610fe264c
Alan Viana <alanviana.nascimento@gmail.com>
2023-02-11 19:12:23 -0300

    chore: job update

---
+++ atualiza_banco_dados.py
@@ -0,0 +1,32 @@
+from src.busca_dados_brapi import get_crypto_by_sigla, get_acao_by_sigla, get_moeda_by_sigla
+from src.inseri_dados_mapa_cotacoes import inseri_dados_crypto, inseri_dados_moeda, inseri_dados_acoes
+
+
+CRYPTOS = ["BTC", "ETH", "LTC", "BNB", "DOGE", "AVAX", "BCH", "USDC", "BUSD"]
+ACOES = ["EMBR3", "PETR4", "MGLU3"]
+MOEDAS = ["USD", "EUR", "ARS", "BOB", "CNY", "PYG", "VEF"]
+
+
+def buscar_e_inserir_dados_iniciais():
+    for crypto in CRYPTOS:
+        ativo = get_crypto_by_sigla(crypto)
+        inseri_dados_crypto(ativo.toJson())
+
+    for acao in ACOES:
+        ativo = get_acao_by_sigla(acao)
+        inseri_dados_acoes(ativo.toJson())
+
+    for moeda in MOEDAS:
+        ativo = get_moeda_by_sigla(moeda)
+        inseri_dados_moeda(ativo.toJson())
+
+
+def buscar_e_inserir_cotacoes():
+    print("Buscando e armazenando as cotações")
+    for crypto in CRYPTOS:
+        # TODO()
+        pass
+
+    for acao in ACOES:
+        # TODO()
+        pass
--- main.py
+++ main.py
@@ -1,45 +1,20 @@
+from flask import Flask
 import os
-from src.busca_dados_brapi import get_crypto_by_sigla, get_acao_by_sigla, get_moeda_by_sigla
-from src.inseri_dados_mapa_cotacoes import inseri_dados_crypto, inseri_dados_moeda, inseri_dados_acoes
+from atualiza_banco_dados import buscar_e_inserir_dados_iniciais, buscar_e_inserir_cotacoes
 
 INSERIR_DADOS_INICIAIS = os.environ.get("INSERIR_DADOS_INICIAIS", "true")
 
-CRYPTOS = ["BTC", "ETH", "LTC", "BNB", "DOGE", "AVAX", "BCH", "USDC", "BUSD"]
-ACOES = ["EMBR3", "PETR4", "MGLU3"]
-MOEDAS = ["USD", "EUR", "ARS", "BOB", "CNY", "PYG", "VEF"]
+app = Flask(__name__)
 
 
-def buscar_e_inserir_dados_iniciais():
-    for crypto in CRYPTOS:
-        ativo = get_crypto_by_sigla(crypto)
-        inseri_dados_crypto(ativo.toJson())
-
-    for acao in ACOES:
-        ativo = get_acao_by_sigla(acao)
-        inseri_dados_acoes(ativo.toJson())
-
-    for moeda in MOEDAS:
-        ativo = get_moeda_by_sigla(moeda)
-        inseri_dados_moeda(ativo.toJson())
-
-
-def buscar_e_inserir_cotacoes():
-    print("Buscando e armazenando as cotações")
-    for crypto in CRYPTOS:
-        # TODO()
-        pass
-
-    for acao in ACOES:
-        # TODO()
-        pass
-
-
-def main():
+@app.route("/")
+def update():
     if INSERIR_DADOS_INICIAIS == "true":
         buscar_e_inserir_dados_iniciais()
 
     buscar_e_inserir_cotacoes()
+    return "Cotacoes atualizadas"
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
